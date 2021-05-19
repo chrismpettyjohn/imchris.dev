d90425f89acbf0e8f1d163e039490aac6d299f82
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-05-18 19:58:07 -0500

    Scaffold Material app

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SitePages } from './pages/SitePages';
+import { Grid, CssBaseline } from '@material-ui/core';
+import { SiteSideBar } from './components/site-sidebar/SiteSidebar';
+
+
+function App() {
+  return (
+    <CssBaseline>
+      <Grid container spacing={4}>
+        <Grid item xs={2} style={{position: 'relative'}}>
+          <SiteSideBar />
+        </Grid>
+        <Grid item xs={10}>
+          <SitePages />
+        </Grid>
+      </Grid>
+    </CssBaseline>
+  );
+}
+
+export default App;
---
+++ src/pages/work/WorkPage.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card, CardActionArea, CardContent, CardMedia, createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      background: '#33691E',
+      color: 'white',
+      flexGrow: 1,
+      borderRadius: 0,
+      padding: 4
+    },
+    media: {
+      height: 500
+    },
+  }),
+);
+
+
+export function WorkPage() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} id="home-page">
+      <CardActionArea>
+        <CardContent>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Typography variant="h3">
+                Previous Work
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Card elevation={2}>
+                <CardActionArea>
+                  <CardMedia className={classes.media} image="https://www.marketingmilk.com/wp-content/themes/marketing-milk/static/img/LaptopV2.png" />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Marketing Milk
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                      across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
